487aa0c39dd9c2cd6bc17d0af6dd7fc0870dfdb1
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-02-06 10:56:01 -0300

    Agregar: ejercicios de bucle for

---
+++ CadenaDeTexto.py
@@ -0,0 +1,7 @@
+texto = input("Escribe un texto: ")
+contador = 0
+
+for i in texto:
+    contador += 1
+    
+print("El texto tiene", contador, "caracteres")
---
+++ Lista.py
@@ -0,0 +1,7 @@
+list = [27, 5, 0, 16, 54, 87, 98, 2, 47, 2]
+suma = 0
+
+for i in list:
+    suma += i
+
+print(suma)
