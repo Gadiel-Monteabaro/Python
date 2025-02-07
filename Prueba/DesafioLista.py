eeaebf9c2e2188199d520262e3a299e842fe146d
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-02-07 18:23:41 -0300

    Desafio Lista

---
+++ Prueba/DesafioLista.py
@@ -0,0 +1,20 @@
+amigos = ['Ana', 'Monica', 'José', 'Camila', 'Raquel', 'Matías']
+
+# Devuelve la posición (el index, un número) del amigo “Matías”.
+print("La posicion de Matías es ", amigos.index('Matías'))
+
+# Devuelve la misma lista pero añadiendo los amigos de la infancia “Ivana” y “Andrés” como otra lista.
+amigosInfancia = ["Ivana", "Andres"]
+amigos.extend(amigosInfancia)
+
+# Agrega un nuevo amigo “María” y devuelve el nro de amigos
+amigos.append("María")
+print(len(amigos))
+
+# Elimina el último elemento amigo y devuelve el nombre del amigo eliminado
+amigoEliminado = amigos.pop()
+print("El amigo eliminado es ", amigoEliminado)
+
+# Devuelve un arreglo ordenado alfabéticamente
+amigos.sort()
+print("Amigos ordenados alfabeticamente ", amigos)
