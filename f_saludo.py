85e4a8d4d98e5916531a80d1370bbd2802896f15
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-02-14 20:49:45 -0300

    actualizacion

---
+++ f_saludo.py
@@ -0,0 +1,8 @@
+# Escribe una función que reciba como parámetros un nombre y devuelva un saludo personalizado
+
+name = input("¿Cual es tu nombre? \n")
+
+def saludo(name):
+  return f"Hola, {name}!"
+
+print(saludo(name))
