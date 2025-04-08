886e0e5104d14dc5d70ced1cf57b94001542d793
Gadiel Monteabaro <silvagadiel96@gmail.com>
2025-04-07 21:05:21 -0300

    a

--- Practica/6_numero_primo.py
+++ Practica/6_numero_primo.py
@@ -4,6 +4,4 @@ numero = int(input("Ingresar un número: "))
 if numero % numero and numero % 1 == 0:
   print("El número", numero, "es primo")
 else:
-  print("El número", numero, "no es primo")
-  
-  
+  print("El número", numero, "no es primo")  
